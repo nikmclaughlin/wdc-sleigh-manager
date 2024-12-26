8764bde618d672832c2994c3fa51d3a4f44b6909
Nik McLaughlin <72171383+nikmclaughlin@users.noreply.github.com>
2024-12-26 14:02:25 -0800

    provide Sleigh type

--- frontend/sanity/queries.ts
+++ frontend/sanity/queries.ts
@@ -1,6 +1,16 @@
 import { Sanity } from './client'
 
-type Sleigh = {}
+type Sleigh = {
+  maintenanceHistory: []
+  mileage: number
+  model: string
+  name: string
+  _createdAt: string
+  _id: string
+  _rev: string
+  _type: 'sleigh'
+  _updatedAt: string
+}
 
 export async function getSleighs() {
   const sleighs: Sleigh[] = await Sanity.fetch('*[_type == "sleigh"]')
